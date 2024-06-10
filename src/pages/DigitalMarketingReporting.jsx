6a430a8b75bb4230baaf42adfd3e90e88d926dea
Samir1113 <94626926+Samir1113@users.noreply.github.com>
2024-06-10 22:40:18 +0300

    added the rest of reporting

--- src/pages/DigitalMarketingReporting.jsx
+++ src/pages/DigitalMarketingReporting.jsx
@@ -1,9 +1,305 @@
+import { useEffect } from "react";
+import BgArea from "../components/BgArea";
+import { Link } from "react-router-dom";
+import ContactForm from "../components/ContactForm";
 function DigitalMarketingReporting() {
-    return (
-        <div>
-            Digital Marketing Reporting
+  useEffect(() => {
+    document.title = "Digital Marketing Reporting";
+  }, []);
+  return (
+    <>
+      <BgArea
+        title1={`Digital Marketing Reporting`}
+        title2={`Reporting`}
+        path={`/reporting`}
+      />
+      <div className="service-details pages pt-90 pb-50">
+        <div className="container">
+          <div className="row">
+            <div className=" col-lg-4 col-md-5 col-sm-12 col-xs-12">
+              <h3 className="pb-15">Our Services:</h3>
+              <div className="service-details-pn-list">
+                <ul>
+                  <li>
+                    <Link to="/social-media-marketing">
+                      Social Media Marketing
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/social-media-management">
+                      Social Media Management
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/hotel-e-commerce">
+                      Hotel E-commerce
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/search-engine-optimization">
+                      Search Engine Optimization
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/email-marketing">
+                      Email Marketing
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/reporting">
+                      Reporting
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/hospitality-photography">
+                      Hospitality Photography
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/the-hotels-network">
+                      The Hotels Network
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/asksuite-hotel-chatbot">
+                      Asksuite Hotel Chatbot
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="service-details-big-button mt-40 mb-40">
+                <a href="/">
+                  <span className="details-big-content">
+                    Company Presentation <i className="fa fa-download"></i>
+                  </span>
+                </a>
+              </div>
+
+              <div
+                className="service-details-pn-about mb-4"
+                style={{ backgroundImage: "url(assets/images/tab1.jpg)" }}
+              >
+                <div className="service-details-pn-about-content pt-35 pb-40 pl-4 pr-4">
+                  <div className="service-details-pn-about-content-title pb-3">
+                    <h4>Need Any Help For Business ?</h4>
+                  </div>
+                  <div className="service-details-pn-about-content-text">
+                    <p>
+                      We take an omni-channel approach to online marketing with
+                      a focus on increasing conversions, repeat traffic and
+                      generating greater brand loyalty online.
+                    </p>
+                  </div>
+                  <div className="service-details-pn-about-content-button pt-2">
+                    <Link to="/contact-us">Contact Now</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-8 col-md-7 col-sm-12 col-xs-12">
+              <div className="row justify-content-center">
+                <div className="col-lg-12 col-md-12 col-sm-12">
+                  <div className="service-main-details">
+                    <div className="service-main-details-inner">
+                      <div className="service-main-details-inner-thumb">
+                        <img
+                          src="assets/images/digital-marketing-reporting---digital-marketing-analytics---hotel-digital-marketing-reports-20212510140712.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div className="service-main-details-content-title pt-4 pb-3">
+                        <h3>Digital Marketing Reporting</h3>
+                      </div>
+
+                      <div className="service-main-details-content-text pb-4">
+                        <h4 className="pb-15"
+                          style={{
+                            margin: "0 0 15px;",
+                            fontFamily: "open sans, sans-serif;",
+                          }}
+                        >
+                          It is all about digital marketing reporting.
+                        </h4>
+                        <p
+                          style={{
+                            margin: "0 0 15px;",
+                            fontFamily: "open sans, sans-serif;",
+                            lineHeight: "31px",
+                          }}
+                        >
+                          We provide you with consultancy in choosing the best
+                          tool to use for your digital marketing reporting
+                          objectives. Do you need to track bookings? from where
+                          they occur? When do they occur? Which nationalities?
+                          It is all possible with T.I.T smart digital solutions
+                          and endless effective tools.
+                        </p>
+
+                        <div className="py-3">
+                          <h4 className="pb-20">
+                            We will help you in the following:
+                          </h4>
+                          <ul class="list-bullets">
+                            <li>
+                              Google Analytics Implementation and Consultancy.
+                            </li>
+                            <li>Heat Maps Implementation and Consultancy.</li>
+                            <li>SEO reporting.</li>
+                            <li>Competitors Intelligence Reporting.</li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="service-main-details-content-title pt-4 pb-3">
+                    <h3>REPORTING:</h3>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-6">
+                  <div className="techno_flipbox mb-30">
+                    <div className="techno_flipbox_font">
+                      <div className="techno_flipbox_inner">
+                        <div className="techno_flipbox_icon">
+                          <div className="icon">
+                            <i className="flaticon-intelligent"></i>
+                          </div>
+                        </div>
+                        <div className="flipbox_title">
+                          <h3>Google Analytics Implementation</h3>
+                        </div>
+                        <div className="flipbox_desc">
+                          <p>
+                            We align your business goals with the implementation...
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      className="techno_flipbox_back "
+                      style={{
+                        backgroundImage:
+                          "url(assets/images/google-analytics-for-hotels---google-website-analytics---hotel-website-analytics-20212510265317.jpg)",
+                        backgroundPosition: "center center",
+                        backgroundSize: "cover",
+                      }}
+                    >
+                      <div className="techno_flipbox_inner">
+                        <div className="flipbox_title">
+                          <h3>Google Analytics Implementation</h3>
+                        </div>
+                        <div className="flipbox_desc">
+                          <p>
+                            We align your business goals with the implementation..
+                          </p>
+                        </div>
+                        <div className="flipbox_button">
+                          <Link to="/travel-tours">
+                            Read More
+                            <i className="fa fa-angle-double-right"></i>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-6">
+                  <div className="techno_flipbox mb-30">
+                    <div className="techno_flipbox_font">
+                      <div className="techno_flipbox_inner">
+                        <div className="techno_flipbox_icon">
+                          <div className="icon">
+                            <i className="flaticon-intelligent"></i>
+                          </div>
+                        </div>
+                        <div className="flipbox_title">
+                          <h3>Website 360 Reporting</h3>
+                        </div>
+                        <div className="flipbox_desc">
+                          <p>
+                            We offer you a 360 website reporting that is
+                            designed for customizable data views, powerful
+                            analysis, and actionable information...
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      className="techno_flipbox_back "
+                      style={{
+                        backgroundImage:
+                          "url(assets/images/google-analytics-360---reporting-360-202125104654433.jpg)",
+                        backgroundPosition: "center center",
+                        backgroundSize: "cover",
+                      }}
+                    >
+                      <div className="techno_flipbox_inner">
+                        <div className="flipbox_title">
+                          <h3>Website 360 Reporting</h3>
+                        </div>
+                        <div className="flipbox_desc">
+                          <p>
+                            We offer you a 360 website reporting that is
+                            designed for customizable data views, powerful
+                            analysis, and actionable information..
+                          </p>
+                        </div>
+                        <div className="flipbox_button">
+                          <Link to="/travel-tours">
+                            Read More
+                            <i className="fa fa-angle-double-right"></i>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+
+      {/*talk to experts section*/}
+      <ContactForm />
+    </>
+  );
 }
 
-export default DigitalMarketingReporting
+export default DigitalMarketingReporting;
--- src/pages/Website360Reporting.jsx
+++ src/pages/Website360Reporting.jsx
@@ -1,9 +1,328 @@
+import { useEffect } from "react";
+import BgArea from "../components/BgArea";
+import { Link } from "react-router-dom";
+import ContactForm from "../components/ContactForm";
 function Website360Reporting() {
-    return (
-        <div>
-            Website 360 Reporting
+  useEffect(() => {
+    document.title = "Website 360 Reporting";
+  }, []);
+  return (
+    <>
+      <BgArea
+        title1={`Website 360 Reporting`}
+        title2={`Reporting`}
+        path={`/reporting`}
+      />
+      <div className="service-details pages pt-90 pb-50">
+        <div className="container">
+          <div className="row">
+            <div className=" col-lg-4 col-md-5 col-sm-12 col-xs-12">
+              <h3 className="pb-15">Our Services:</h3>
+              <div className="service-details-pn-list">
+                <ul>
+                  <li>
+                    <Link to="/social-media-marketing">
+                      Social Media Marketing
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/social-media-management">
+                      Social Media Management
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/hotel-e-commerce">
+                      Hotel E-commerce
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/search-engine-optimization">
+                      Search Engine Optimization
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/email-marketing">
+                      Email Marketing
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/reporting">
+                      Reporting
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/hospitality-photography">
+                      Hospitality Photography
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link to="/the-hotels-network">
+                      The Hotels Network
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/asksuite-hotel-chatbot">
+                      Asksuite Hotel Chatbot
+                      <span>
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="service-details-big-button mt-40 mb-40">
+                <a href="/">
+                  <span className="details-big-content">
+                    Company Presentation <i className="fa fa-download"></i>
+                  </span>
+                </a>
+              </div>
+
+              <div
+                className="service-details-pn-about mb-4"
+                style={{ backgroundImage: "url(assets/images/tab1.jpg)" }}
+              >
+                <div className="service-details-pn-about-content pt-35 pb-40 pl-4 pr-4">
+                  <div className="service-details-pn-about-content-title pb-3">
+                    <h4>Need Any Help For Business ?</h4>
+                  </div>
+                  <div className="service-details-pn-about-content-text">
+                    <p>
+                      We take an omni-channel approach to online marketing with
+                      a focus on increasing conversions, repeat traffic and
+                      generating greater brand loyalty online.
+                    </p>
+                  </div>
+                  <div className="service-details-pn-about-content-button pt-2">
+                    <Link to="/contact-us">Contact Now</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-8 col-md-7 col-sm-12 col-xs-12">
+              <div className="row justify-content-center">
+                <div className="col-lg-12 col-md-12 col-sm-12">
+                  <div className="service-main-details">
+                    <div className="service-main-details-inner">
+                      <div className="service-main-details-inner-thumb">
+                        <img
+                          src="assets/images/google-analytics-360---reporting-360-202125104654433.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div className="service-main-details-content-title pt-4 pb-3">
+                        <h3>Google Analytics 360</h3>
+                      </div>
+
+                      <div className="service-main-details-content-text pb-4">
+                        <p
+                          style={{
+                            margin: "0 0 15px;",
+                            fontFamily: "open sans, sans-serif;",
+                            lineHeight: "31px",
+                          }}
+                        >
+                          Great data is that can be used to improve your
+                          business; with T.I.T Solutions, get the vital data you
+                          need, whenever and however you need it!
+                        </p>
+
+                        <p
+                          style={{
+                            margin: "0 0 15px;",
+                            fontFamily: "open sans, sans-serif;",
+                            lineHeight: "31px",
+                          }}
+                        >
+                          We offer you google analytics 360 reports designed for
+                          customizable data views, powerful analysis, and
+                          actionable information.
+                        </p>
+                        <p
+                          style={{
+                            margin: "0 0 15px;",
+                            fontFamily: "open sans, sans-serif;",
+                            lineHeight: "31px",
+                          }}
+                        >
+                          Identify your audience and know who visited your
+                          website, and turn them into engaged shoppers. We track
+                          all visitors, filtering out irrelevant parties, so you
+                          only get leads that are relevant for you:
+                        </p>
+
+                        <div className="py-3">
+                          <ul class="list-bullets">
+                            <li>When they last visited your website</li>
+                            <li>The specific pages they visited</li>
+                            <li>How long they spent on your site</li>
+                            <li>The marketing channel they came from</li>
+                            <li>The action they took on your website</li>
+                          </ul>
+                        </div>
+                        <p
+                          style={{
+                            margin: "0 0 15px;",
+                            fontFamily: "open sans, sans-serif;",
+                            lineHeight: "31px",
+                          }}
+                        >
+                          Our intuitive, easy-to-read, and seamless reports will
+                          help you gain the knowledge and the insights you need
+                          about what is happening on your site and which online
+                          activities matter the most. With our reporting 360
+                          advanced services, you can manage everything,
+                          including Facebook Ads, Google Ads, LinkedIn Ads,
+                          Instagram, and you can easily connect to the required
+                          marketing channels within minutes.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="service-main-details-content-title pt-4 pb-3">
+                    <h3>REPORTING:</h3>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-6">
+                  <div className="techno_flipbox mb-30">
+                    <div className="techno_flipbox_font">
+                      <div className="techno_flipbox_inner">
+                        <div className="techno_flipbox_icon">
+                          <div className="icon">
+                            <i className="flaticon-intelligent"></i>
+                          </div>
+                        </div>
+                        <div className="flipbox_title">
+                          <h3>Google Analytics Implementation</h3>
+                        </div>
+                        <div className="flipbox_desc">
+                          <p>
+                            We align your business goals with the
+                            implementation...
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      className="techno_flipbox_back "
+                      style={{
+                        backgroundImage:
+                          "url(assets/images/google-analytics-for-hotels---google-website-analytics---hotel-website-analytics-20212510265317.jpg)",
+                        backgroundPosition: "center center",
+                        backgroundSize: "cover",
+                      }}
+                    >
+                      <div className="techno_flipbox_inner">
+                        <div className="flipbox_title">
+                          <h3>Google Analytics Implementation</h3>
+                        </div>
+                        <div className="flipbox_desc">
+                          <p>
+                            We align your business goals with the
+                            implementation..
+                          </p>
+                        </div>
+                        <div className="flipbox_button">
+                          <Link to="/travel-tours">
+                            Read More
+                            <i className="fa fa-angle-double-right"></i>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-6">
+                  <div className="techno_flipbox mb-30">
+                    <div className="techno_flipbox_font">
+                      <div className="techno_flipbox_inner">
+                        <div className="techno_flipbox_icon">
+                          <div className="icon">
+                            <i className="flaticon-intelligent"></i>
+                          </div>
+                        </div>
+                        <div className="flipbox_title">
+                          <h3>Digital Marketing Reporting</h3>
+                        </div>
+                        <div className="flipbox_desc">
+                          <p>
+                            We provide you with consultancy in choosing the best tool to use for your digital marketing reporting objectives...
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      className="techno_flipbox_back "
+                      style={{
+                        backgroundImage:
+                          "url(assets/images/digital-marketing-reporting---digital-marketing-analytics---hotel-digital-marketing-reports-20212510140712.jpg)",
+                        backgroundPosition: "center center",
+                        backgroundSize: "cover",
+                      }}
+                    >
+                      <div className="techno_flipbox_inner">
+                        <div className="flipbox_title">
+                          <h3>Digital Marketing Reporting</h3>
+                        </div>
+                        <div className="flipbox_desc">
+                          <p>
+                            We provide you with consultancy in choosing the best tool to use for your digital marketing reporting objectives...
+                          </p>
+                        </div>
+                        <div className="flipbox_button">
+                          <Link to="/hotels-resorts">
+                            Read More
+                            <i className="fa fa-angle-double-right"></i>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+
+      {/*talk to experts section*/}
+      <ContactForm />
+    </>
+  );
 }
 
-export default Website360Reporting
+export default Website360Reporting;
