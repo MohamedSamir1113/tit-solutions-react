ac2776bc26d7d319d99f7f30216470c492126264
MohamedSamir1113 <simo2000854@gmail.com>
2024-06-25 15:25:35 +0200

    added the right loader

--- src/components/DEdgeSolutionsList.jsx
+++ src/components/DEdgeSolutionsList.jsx
@@ -16,66 +16,66 @@ function DEdgeSolutionsList() {
     return () => clearTimeout(timer);
   }, [loading, dispatch]);
   return (
-    <div className=" col-lg-4 col-md-5 col-sm-12 col-xs-12">
-      <h3 className="pb-15">OTHER D-EDGE SOLUTIONS</h3>
-      <div className="service-details-pn-list">
-        <ul>
-          <li>
-            <Link
-              onClick={() => dispatch(isLoading(true))}
-              to="/d-edge/guest-management"
-            >
-              Guest Management
-              <span>
-                <i className="fa fa-angle-right"></i>
-              </span>
-            </Link>
-          </li>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className=" col-lg-4 col-md-5 col-sm-12 col-xs-12">
+          <h3 className="pb-15">OTHER D-EDGE SOLUTIONS</h3>
+          <div className="service-details-pn-list">
+            <ul>
+              <li>
+                <Link onClick={() => dispatch(isLoading(true))} to="/d-edge/guest-management">
+                  Guest Management
+                  <span>
+                    <i className="fa fa-angle-right"></i>
+                  </span>
+                </Link>
+              </li>
 
-          <li>
-            <Link
-              onClick={() => dispatch(isLoading(true))}
-              to="/d-edge/data-intelligence"
-            >
-              Data Intelligence
-              <span>
-                <i className="fa fa-angle-right"></i>
+              <li>
+                <Link onClick={() => dispatch(isLoading(true))} to="/d-edge/data-intelligence">
+                  Data Intelligence
+                  <span>
+                    <i className="fa fa-angle-right"></i>
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="service-details-big-button mt-40 mb-40">
+            <a href="/">
+              <span className="details-big-content">
+                Company Presentation <i className="fa fa-download"></i>
               </span>
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="service-details-big-button mt-40 mb-40">
-        <a href="/">
-          <span className="details-big-content">
-            Company Presentation <i className="fa fa-download"></i>
-          </span>
-        </a>
-      </div>
+            </a>
+          </div>
 
-      <div
-        className="service-details-pn-about mb-4"
-        style={{ backgroundImage: "url(assets/images/tab1.jpg)" }}
-      >
-        <div className="service-details-pn-about-content pt-35 pb-40 pl-4 pr-4">
-          <div className="service-details-pn-about-content-title pb-3">
-            <h4>Need Any Help For Business ?</h4>
-          </div>
-          <div className="service-details-pn-about-content-text">
-            <p>
-              We take an omni-channel approach to online marketing with a focus
-              on increasing conversions, repeat traffic and generating greater
-              brand loyalty online.
-            </p>
-          </div>
-          <div className="service-details-pn-about-content-button pt-2">
-            <Link className="list-color" to="/contact-us">
-              Contact Now
-            </Link>
+          <div
+            className="service-details-pn-about mb-4"
+            style={{ backgroundImage: "url(assets/images/tab1.jpg)" }}
+          >
+            <div className="service-details-pn-about-content pt-35 pb-40 pl-4 pr-4">
+              <div className="service-details-pn-about-content-title pb-3">
+                <h4>Need Any Help For Business ?</h4>
+              </div>
+              <div className="service-details-pn-about-content-text">
+                <p>
+                  We take an omni-channel approach to online marketing with a
+                  focus on increasing conversions, repeat traffic and generating
+                  greater brand loyalty online.
+                </p>
+              </div>
+              <div className="service-details-pn-about-content-button pt-2">
+                <Link className="list-color" to="/contact-us">
+                  Contact Now
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
